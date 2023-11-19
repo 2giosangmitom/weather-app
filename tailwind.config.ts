ce5625db5e13b1e5ef8405fca8d6c0b374569d47
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-19 20:09:09 +0700

    init new design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      keyframes: {
-        shake: {
-          "0%": {
-            transform: "translate(3px, 0)",
-          },
-          "50%": {
-            transform: "translate(-3px, 0)",
-          },
-          "100%": {
-            transform: "translate(0, 0)",
-          },
-        },
-      },
-      animation: {
-        shake: "shake 100ms ease-in-out infinite",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
